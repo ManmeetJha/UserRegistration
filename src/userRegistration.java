a065cbac20afeab4b93ceeae62a9bac9cd992ca7
Navneet <navneetjh5@gmail.com>
2020-10-04 22:10:41 +0530

    User Registration printed

---
+++ src/userRegistration.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class userRegistration {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("User Registration");
+
+	}
+
+}
