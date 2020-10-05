fe7fa4e5a5b4c2e106f41e39a4ae66bf2e1d7fe8
Navneet <navneetjh5@gmail.com>
2020-10-05 19:07:53 +0530

    Phone Number check

--- src/userRegistration.java
+++ src/userRegistration.java
@@ -18,7 +18,17 @@ public class userRegistration {
 		System.out.println("Enter the last name");
 		String lname = input.next();
 		System.out.println(Pattern.matches("^[A-Z](?:[a-zA-Z]+){2}$", lname));
-		input.close();
+
+		// UC3
+		System.out.println("Enter valid e-mail ID");
+		String email = input.next();
+		System.out.println(Pattern.matches("^[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)+$", email));
+
+		// UC4
+		System.out.println("Enter valid Phone number");
+		String phone = input.next();
+		System.out.println(Pattern.matches("(0|91)?[7-9][0-9]{9}", phone));
+
 	}
 
 }
